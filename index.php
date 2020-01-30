1eb92007f1956c92c43ece9b2257905f41fa699d
Faver <faverl@misena.edu.co>
2020-01-30 00:05:57 -0500

    Avance de curso, manejo de variables

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
 <html>
+	
+	<head>
+		
+	</head>
+	
+	<body>
+		<?php
+			$nombre="Faver";
+			$apellido="Lopez";
+		
+			echo $nombre . " " . $apellido;
+		
+		?>
+	</body>
 
 </html>
