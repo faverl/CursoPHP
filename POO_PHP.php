66dbe0265fc760c96a9956469a00c51d3fc12d30
Faver <faverl@misena.edu.co>
2020-02-02 01:07:48 -0500

    Se crea clase vehiculo con parametros y funciones

--- POO_PHP.php
+++ POO_PHP.php
@@ -9,7 +9,31 @@
 	
 	<?php
 	
-	
+		class Vehiculo{
+
+			var $color;	
+			var $motor;
+			
+			function Vehiculo(){ //Metodo Contructor;
+				
+				$this->color="";
+				$this->motor="1600cc";
+			}
+
+			function acelerar(){
+				
+				echo("Se esta acelerando");
+
+			}
+
+			function frenar(){
+				echo("Se esta frenando");
+			}
+
+			function girar(){
+				echo("Se esta girando");
+			}
+		}
 	
 	
 	?>
