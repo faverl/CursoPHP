325bbf0f7c534d25092b73cc07cf898c80781871
Faver <faverl@misena.edu.co>
2020-02-02 18:35:20 -0500

    Se agrega información sobre el manejo de array multidimencional

--- Array_PHP.php
+++ Array_PHP.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Documento sin título</title>
+<title>Manejo de Array</title>
 </head>
 
 <body>
 	
 	<?php // Utlizacion de Arrays
 		
-		/*Array Asociativo*/
+		/*ARRAY INDEXADO*/
 	
 		$semana[] = "Lunes";
 		$semana[] = "Martes";
@@ -19,12 +19,62 @@
 		$semana[] = "Sabado";
 		$semana[] = "Domingo";
 	
-		//echo $semana[1];
+		//Ordenar en orden alfabetico
+	
+		sort($semana);
 	
 		foreach($semana as $dia){
 			
 			echo $dia . "<br>";
 		}
+		
+		print "<br>";
+	
+	
+	
+		/* ARRAY ASOCIATIVO */
+	
+		$datos=array("nombre"=>"Faver", "apellido"=>"Lopez", "edad"=>27);
+	
+		// Validacion de array
+	
+		if(is_array($datos)){
+			
+			echo " Es un array <br>";
+		}
+		else{
+			
+			echo " No es un array <br>";
+		}
+	
+		print "<br>";
+	
+		foreach($datos as $clave => $valor){
+			
+			echo $clave. ": " . $valor . "<br>";
+		}
+	
+		print "<br>";
+		
+		/* ARRAY MULTIDIMENCIONAL */
+	
+		$alimentos=array("frutas"=>array("kiwi","mandarina", "manzana"),
+						 "leche"=>array("vaca", "coco" ,"cabra"),
+						 "carne"=>array("lomo", "pata", "costilla"));
+	
+		//echo $alimentos["leche"][0];
+	
+		foreach($alimentos as $clave_alim=>$alim){
+			
+			echo "<b>$clave_alim :</b> <br><br>";
+			
+			foreach($alim as $clave=>$valor){
+				echo $clave+1  . ": $valor. <br>";
+			}
+			
+			echo "<br>";
+		}
+		
 	?>
 </body>
 </html>
