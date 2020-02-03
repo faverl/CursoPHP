68b6b02427b78e149e70a1ca9f43374ade263be5
Faver <faverl@misena.edu.co>
2020-02-02 19:59:57 -0500

    1. Se crea archivo data_base.php, para el manejo de bases de datos. \n2. Se agrega codigo para reaizar conexión a la base de datos y realizar consulta. \n3. Se agrega lineas de codigo para recorrer la matriz de resultado utilizando while y foreach.

---
+++ data_base.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+	
+	<?php
+		
+		$db_host="localhost";
+		$db_name="curso_php";
+		$db_user="root";
+		$db_pass="";
+
+		//Se genera la conexion a la DB.
+		$conexion=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+		//Validar conexion a la DB
+		if(mysqli_connect_errno()){
+
+			echo "Falla al conectar la base de datos";
+
+			exit();
+		}
+
+		// Para que cononozca acentos.
+		mysqli_set_charset($conexion,"utf-8");
+
+		//Consulta a la DB.
+		$query = "select * from usuarios";
+
+		//Resultado de la consulta
+		$resulset = mysqli_query($conexion, $query);
+
+		//Se guarda la siguiente fila del resultado
+		//$fila = mysqli_fetch_row($resulset);
+		
+		//Se recorre toda la matriz de resultados.
+		while(($fila=mysqli_fetch_row($resulset))){
+
+			foreach($fila as $clave=>$valor){
+
+				echo " $valor ";
+			}
+			
+			echo "<br>";
+		}
+	
+	?>
+	
+	
+</body>
+</html>
